d80b9f5aa6fa3e2b13ad567b99dc8f6c5d6d0dba
mdadadan <masuda@heyagoto.co.jp>
2025-12-23 15:03:39 +0900

    Create middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const password = process.env.BASIC_AUTH_PASSWORD;
+
+  if (!password) return NextResponse.next();
+
+  const auth = req.headers.get("authorization");
+
+  if (!auth) {
+    return new NextResponse("Auth required", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Protected"',
+      },
+    });
+  }
+
+  const [, encoded] = auth.split(" ");
+  const decoded = Buffer.from(encoded, "base64").toString();
+  const [, pass] = decoded.split(":");
+
+  if (pass !== password) {
+    return new NextResponse("Forbidden", { status: 403 });
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next|favicon.ico).*)"],
+};
